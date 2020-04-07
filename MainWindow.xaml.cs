309873897096d83ee36bfa8bb165d0b1376db9e5
PalasDogan <63246904+PalasDogan@users.noreply.github.com>
2020-04-07 16:49:57 +0300

    Menü butunları ve birkaç düzenleme

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 namespace WpfApp7
 {
     /// <summary>
@@ -22,12 +24,98 @@ namespace WpfApp7
     {
         public MainWindow()
         {
-            InitializeComponent();
+            //InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void chan_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+       
+       
+        private void chan_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            chan.Text = "";
+        }
+
+        private void kampanyalar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            kampanyalar.Background = new SolidColorBrush(Colors.ForestGreen);
+            kampanyalar.Foreground = new SolidColorBrush(Colors.White);
+            kampanyalar.Height = kampanyalar.Height + 5;
+            kampanyalar.Width = kampanyalar.Width + 5;
+            kampanyalar.FontSize = kampanyalar.FontSize +4;
+        }
+
+        private void kampanyalar_MouseLeave(object sender, MouseEventArgs e)
+        {
+            kampanyalar.Background = new SolidColorBrush(Colors.White);
+            kampanyalar.Foreground = new SolidColorBrush(Colors.Black);
+            kampanyalar.Height = kampanyalar.Height -5;
+            kampanyalar.Width = kampanyalar.Width - 5;
+            kampanyalar.FontSize = kampanyalar.FontSize - 4;
+        }
+
+        
+        private void cokSatanlar_MouseLeave(object sender, MouseEventArgs e)
+        {
+            cokSatanlar.Background = new SolidColorBrush(Colors.White);
+            cokSatanlar.Foreground = new SolidColorBrush(Colors.Black);
+            cokSatanlar.Height = cokSatanlar.Height - 5;
+            cokSatanlar.Width = cokSatanlar.Width - 5;
+            cokSatanlar.FontSize = cokSatanlar.FontSize - 4;
+        }
+
+        private void cokSatanlar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            cokSatanlar.Background = new SolidColorBrush(Colors.ForestGreen);
+            cokSatanlar.Foreground = new SolidColorBrush(Colors.White);
+            cokSatanlar.Height = cokSatanlar.Height + 5;
+            cokSatanlar.Width = cokSatanlar.Width + 5;
+            cokSatanlar.FontSize = cokSatanlar.FontSize + 4;
+        }
+
+        private void firsat_MouseLeave(object sender, MouseEventArgs e)
+        {
+            firsat.Background = new SolidColorBrush(Colors.White);
+            firsat.Foreground = new SolidColorBrush(Colors.Black);
+            firsat.Height = firsat.Height - 5;
+            firsat.Width = firsat.Width - 5;
+            firsat.FontSize = firsat.FontSize - 4;
+        }
+
+        private void firsat_MouseEnter(object sender, MouseEventArgs e)
+        {
+            firsat.Background = new SolidColorBrush(Colors.ForestGreen);
+            firsat.Foreground = new SolidColorBrush(Colors.White);
+            firsat.Height = firsat.Height + 5;
+            firsat.Width = firsat.Width + 5;
+            firsat.FontSize = firsat.FontSize + 4;
+        }
+
+        private void yeni_MouseLeave(object sender, MouseEventArgs e)
+        {
+            yeni.Background = new SolidColorBrush(Colors.White);
+            yeni.Foreground = new SolidColorBrush(Colors.Black);
+            yeni.Height = yeni.Height - 5;
+            yeni.Width = yeni.Width - 5;
+            yeni.FontSize = yeni.FontSize -4;
+        }
+
+        private void yeni_MouseEnter(object sender, MouseEventArgs e)
+        {
+            yeni.Background = new SolidColorBrush(Colors.ForestGreen);
+            yeni.Foreground = new SolidColorBrush(Colors.White);
+            yeni.Height = yeni.Height + 5;
+            yeni.Width = yeni.Width + 5;
+            yeni.FontSize = yeni.FontSize + 4;
+            
         }
     }
 }
